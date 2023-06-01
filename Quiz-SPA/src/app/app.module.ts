890da96d76945f5dfd8d2b25e66c4b81ab7fd3dd
Ahmad Elbeltagy <elbeltagyahmad@gmail.com>
2023-06-01 15:48:46 +0300

    merge conflict

--- Quiz-SPA/src/app/app.module.ts
+++ Quiz-SPA/src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
-=======
-import { HttpClientModule } from '@angular/common/http';
->>>>>>> 3ca69ee45493412f7f082a12bc930da9818f9ed7
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,22 +9,13 @@ import { SubjectpageComponent } from './components/subjectpage/subjectpage.compo
 import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-<<<<<<< HEAD
   declarations: [
     AppComponent,
     LoginComponent,
     SubjectpageComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-=======
-  declarations: [AppComponent, LoginComponent, SubjectpageComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
->>>>>>> 3ca69ee45493412f7f082a12bc930da9818f9ed7
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
