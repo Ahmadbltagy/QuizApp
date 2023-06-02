76ffa8e3a3fb0676bc4c1c03f9c3eabfb4ebb625
Ahmad Elbeltagy <elbeltagyahmad@gmail.com>
2023-06-02 13:05:02 +0300

    Add edit question

--- Quiz-SPA/src/app/app-routing.module.ts
+++ Quiz-SPA/src/app/app-routing.module.ts
@@ -1,17 +1,9 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
-<<<<<<< HEAD
 import { AdminpageComponent } from './components/adminpage/adminpage.component';
 import { ExameditComponent } from './components/examedit/examedit.component';
 
-const routes: Routes =
-[
-  { path: 'register', component: RegisterComponent },
-  {path: 'admin', component:AdminpageComponent},
-  {path: 'admin/:id/edit', component:ExameditComponent}
-
-=======
 import { LoginComponent } from './components/login/login.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { ResultTestComponent } from './components/result-test/result-test.component';
@@ -23,7 +15,8 @@ const routes: Routes = [
   { path: 'about', component: AboutusComponent },
   { path: 'result', component: ResultTestComponent },
   { path: 'subjects', component: SubjectpageComponent },
->>>>>>> 66e3059e90471e85389edbca707f4bae561b58b0
+  { path: 'admin', component: AdminpageComponent },
+  { path: 'admin/:id/edit', component: ExameditComponent },
 ];
 
 @NgModule({
--- Quiz-SPA/src/app/components/examedit/examedit.component.ts
+++ Quiz-SPA/src/app/components/examedit/examedit.component.ts
@@ -11,19 +11,34 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class ExameditComponent implements OnInit {
   subject: any;
   subjectId: any;
+  newQuestion: {
+    text: string;
+    options: any;
+    answer: string;
+  } = { text: '', options: [], answer: '' };
+
+  questionForm = new FormGroup({
+    questionText: new FormControl(''),
+    questionInput: new FormControl(''),
+  });
   constructor(
     private _subjectservice: SubjectService,
     private _activatedroute: ActivatedRoute
   ) {}
   ngOnInit(): void {
     this.subjectId = this._activatedroute.snapshot.paramMap.get('id');
-    this._subjectservice.byId(this.subjectId).subscribe((res) => {
-      this.subject = res;
+    this._subjectservice.byId(this.subjectId).subscribe({
+      next: (res) => (this.subject = res),
     });
   }
-
-  questionForm = new FormGroup({
-    questionText: new FormControl(''),
-    questionInput: new FormControl(''),
-  });
+  //subject[id-1].questions[id-1].options
+  EditQuestoin(e: any) {
+    for (let i = 1; i < e.target.length - 1; i++) {
+      this.newQuestion.options.push(e.target[i].value);
+    }
+    this.newQuestion.text = e.target[0].value;
+    console.log(this.newQuestion);
+    // this._subjectservice.editByid(this.subjectId, this.newQuestion).subscribe();
+    // this.newQuestion = { text: '', options: [], answer: '' };
+  }
 }
