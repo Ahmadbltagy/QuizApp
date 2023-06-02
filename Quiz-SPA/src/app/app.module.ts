66e3059e90471e85389edbca707f4bae561b58b0
Ahmad Elbeltagy <elbeltagyahmad@gmail.com>
2023-06-02 11:17:08 +0300

    merge conflict

--- Quiz-SPA/src/app/app.module.ts
+++ Quiz-SPA/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SubjectpageComponent } from './components/subjectpage/subjectpage.component';
