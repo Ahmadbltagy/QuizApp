9e0e5c688da4b72d7fc827088ddf579952c9b65e
Ahmad Elbeltagy <elbeltagyahmad@gmail.com>
2023-06-02 12:01:39 +0300

    import component into app module

--- Quiz-SPA/src/app/app.module.ts
+++ Quiz-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -11,7 +12,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { ResultTestComponent } from './components/result-test/result-test.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
+import { ExameditComponent } from './components/examedit/examedit.component';
+import { AdminpageComponent } from './components/adminpage/adminpage.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +23,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     ResultTestComponent,
     FooterComponent,
     NavbarComponent,
+    ExameditComponent,
+    AdminpageComponent,
   ],
   imports: [
     BrowserModule,
--- Quiz-SPA/src/app/components/examedit/examedit.component.ts
+++ Quiz-SPA/src/app/components/examedit/examedit.component.ts
@@ -1,27 +1,29 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SubjectService } from 'src/app/services/subject.service';
 import { ActivatedRoute } from '@angular/router';
-import { FormGroup,FormControl,Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-examedit',
   templateUrl: './examedit.component.html',
-  styleUrls: ['./examedit.component.css']
+  styleUrls: ['./examedit.component.css'],
 })
 export class ExameditComponent implements OnInit {
-  subject:any
-  subjectId:any
-  constructor(private _subjectservice:SubjectService,private _activatedroute:ActivatedRoute) {}
+  subject: any;
+  subjectId: any;
+  constructor(
+    private _subjectservice: SubjectService,
+    private _activatedroute: ActivatedRoute
+  ) {}
   ngOnInit(): void {
-    this.subjectId=this._activatedroute.snapshot.paramMap.get("id")
-    this._subjectservice.byId(this.subjectId).subscribe((res)=>{
-      this.subject=res;
-    })
+    this.subjectId = this._activatedroute.snapshot.paramMap.get('id');
+    this._subjectservice.byId(this.subjectId).subscribe((res) => {
+      this.subject = res;
+    });
   }
 
-  questionForm=new FormGroup({
-    questionText:new FormControl(""),
-    questionInput:new FormControl("")
-  })
-
+  questionForm = new FormGroup({
+    questionText: new FormControl(''),
+    questionInput: new FormControl(''),
+  });
 }
